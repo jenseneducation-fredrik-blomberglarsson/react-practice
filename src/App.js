23e18714cee5cc8e13ab7cc1c5ffc9592b869d71
fredrikblomberglarsson <fredrik.blomberg.larsson@gmail.com>
2020-10-15 15:03:56 +0200

    added functional component with react hooks example

--- src/App.js
+++ src/App.js
@@ -40,3 +40,51 @@ class App extends Component {
 }
 
 export default App;
+
+
+// FUNCTIONAL COMPONENT WITH REACT HOOKS EXAMPLE
+
+/* import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Fredrik', age: 25 },
+      { name: 'Peter', age: 45 },
+      { name: 'Gustav', age: 54 }
+    ]
+  });
+
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    console.log('Clicked!');
+    //DON'T do this: this.state.persons[0].name = 'Fredrik Blomberg';
+    setPersonsState({
+      persons: [
+        { name: 'Fredrik Blomberg', age: 25 },
+        { name: 'Peter Berg', age: 45 },
+        { name: 'Gustav Jansson', age: 54 }
+      ]
+    })
+  }
+
+  return (
+    // JSX, looks like HTML but JS in the end. Gets compiled to code underneath (greyed out).
+    <div className="App">
+      <h1>Hi, React App Test</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Show full name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Dancing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+  // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
+}
+
+export default app; */
