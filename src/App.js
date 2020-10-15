62fb0002bf597aa21c36b2aa7b5f8c90b6bfcc87
fredrikblomberglarsson <fredrik.blomberg.larsson@gmail.com>
2020-10-15 14:14:02 +0200

    state & props

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import person from './Person/Person';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Fredrik', age: 25 },
+      { name: 'Peter', age: 45 },
+      { name: 'Gustav', age: 54 }
+    ]
+  }
   render() {
     return (
-      // JSX, looks like HTML but JS in the end. Gets compiled to code underneath.
+      // JSX, looks like HTML but JS in the end. Gets compiled to code underneath (greyed out).
       <div className="App">
         <h1>Hi, React App Test</h1>
         <p>This is really working!</p>
-        <Person name="Fredrik" age="25" />
-        <Person name="Peter" age="45" />
-        <Person name="Gustav" age="54" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Dancing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Does this work now?'));
